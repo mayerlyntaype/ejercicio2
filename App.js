a824bc3a3d5827ee50b4da8f5a9cbf0ff9531934
mayerlyntaype <mayerlyn.taype@tecsup.edu.pe>
2023-04-20 23:00:59 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet,Text,View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+
+const App =()=> {
+  return(
+    <View style ={styles.container}>
+     <View style= {styles.top}>
+      <View style={styles.profileimage}>
+
+      </View>
+     </View>
+      <View style ={styles.center}></View>
+       <View style ={styles.bottom}>
+
+        <View style ={styles.bottomItem}>
+          <View  style ={styles.bottomItemInner}></View>
+
+        </View>
+        <View style ={styles.bottomItem}>
+          <View  style ={styles.bottomItemInner}></View>
+
+        </View>
+        <View style ={styles.bottomItem}>
+          <View  style ={styles.bottomItemInner}></View>
+
+        </View>
+        <View style ={styles.bottomItem}>
+          <View  style ={styles.bottomItemInner}></View>
+
+        </View>
+
+
+       </View>
+
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container :{
+    flex :1
   },
-});
+
+  top: {
+    height:'45%',
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#98d2c1'
+  },
+  profileimage:{
+    width:140,
+    height:140,
+    borderRadius: 100,
+    borderWidth:4,
+    borderColor:'#fff',
+    backgroundColor:'#eee'
+  },
+  center:{
+    height:'10%',
+    backgroundColor:'#7fbcac'
+  },
+  bottom:{
+    height:'45%',
+    backgroundColor:'#292929'
+
+  },
+  bottomItem:{
+    width:'50%',
+    height:'50%',
+    padding:5
+
+  },
+  bottomItemInner:{
+    flex:1,
+    backgroundColor:'#292929'
+  }
+})
+
